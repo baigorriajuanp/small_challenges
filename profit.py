16a067c04cc2d05dd29adbeab8a294495d484546
baigorriajuanp <102814577+baigorriajuanp@users.noreply.github.com>
2023-11-28 16:06:58 -0300

    Add files via upload

---
+++ profit.py
@@ -0,0 +1,21 @@
+"""
+You work for a manufacturer, and have been asked to calculate the total profit made on the sales of a product. 
+You are given a dictionary containing the cost price per unit (in dollars), sell price per unit (in dollars), 
+and the starting inventory. Return the total profit made, rounded to the nearest dollar.
+"""
+
+
+def prof(dictionary):
+    return round(dictionary["inventory"] * (dictionary["sell_price"] - dictionary["cost_price"]))
+
+
+
+profit = ({
+  "cost_price": 32.67,
+  "sell_price": 45.00,
+  "inventory": 1200
+})
+
+
+
+print(prof(profit))
